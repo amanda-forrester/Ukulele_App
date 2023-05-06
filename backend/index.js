65f92cd9a0a4e0d17077df214e1256b4cca4a3d4
Amanda Forrester <amanda.forrester1@gmail.com>
2023-05-05 21:24:00 -0500

    Changed index.js file to reflect vercel server.

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,23 @@
-const express = require('express');
+const { Server } = require('@vercel/node');
+const db = require('./queries');
+
+const server = new Server();
+
+server.get("/api/songs/:chord", async (req, res) => {
+  const { chord } = req.params;
+  try {
+    const songNames = await db.getSongsByChord(chord);
+    res.json({ song_names: songNames });
+  } catch (error) {
+    console.error("Error getting songs:", error);
+    res.status(500).json({ error: "Could not get songs" });
+  }
+});
+
+module.exports = server;
+
+
+/*const express = require('express');
 const port = 3006;
 const db = require('./queries');
 
@@ -19,5 +38,5 @@ app.get("/api/songs/:chord", async (req, res) => {
 
   app.listen(port, () => {
     console.log(`App running on port ${port}.`);
-})
+})*/
   
